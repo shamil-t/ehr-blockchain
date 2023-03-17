30336a386b26d494c4b60ae89e02e054c93b7e02
SHAMIL T <tshamil90@gmail.com>
2023-03-18 01:16:25 +0530

    added pathMatch which is required in angular 15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,25 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  {path: '', component: HomeComponent},
   {
     path: 'admin',
     loadChildren: () =>
-      import('../admin/admin.module').then((m) => m.AdminModule),
+      import('../admin/admin.module').then((m) => m.AdminModule)
   },
   {
     path: 'doctor',
     loadChildren: () =>
       import('../doctor/doctor.module').then((m) => m.DoctorModule),
   },
-  { path: '**', component: HomeComponent, redirectTo: '' },
+  {path: '**', component: HomeComponent, redirectTo: '', pathMatch: "full"},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
