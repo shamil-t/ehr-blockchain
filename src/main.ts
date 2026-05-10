6c280f4bc866476353125c728e2169d835bfdcf7
shamil-t <tshamil90@gmail.com>
2026-05-10 10:03:09 +0530

    upgrade angular 20 to 21

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import {AppModule} from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch(err => console.error(err));
