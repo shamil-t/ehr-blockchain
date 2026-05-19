5752f8bf55a6bbb97b1733a91e08569008cbb96a
shamil-t <tshamil90@gmail.com>
2026-05-19 21:28:13 +0530

    #82 - update homepage design to more professional appearance

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,18 @@
 import {Component, OnInit} from '@angular/core';
-import {NavigationComponent} from "../navigation/navigation.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   imports: [
-    NavigationComponent
+    RouterLink
   ],
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
